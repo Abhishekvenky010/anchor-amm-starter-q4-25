d5b801da3c524dcc1ae7c4dc9452010d0eeead91
Abhishek010 <abhishekvenky010@gmail.com>
2026-02-08 14:32:52 +0530

    done amm

--- programs/anchor-amm-q4-25/src/lib.rs
+++ programs/anchor-amm-q4-25/src/lib.rs
@@ -5,7 +5,7 @@ mod instructions;
 mod state;
 
 use instructions::*;
-declare_id!("9FVkz5L9LZix4zXYmqJLzKBwRpm9aJ539J26UsZSrLWN");
+declare_id!("Cd9JfwGQW1ctq6a9pDyZCfQiujZbvGMeeHYgCs6PRZEP");
 
 #[program]
 pub mod anchor_amm_q4_25 {
